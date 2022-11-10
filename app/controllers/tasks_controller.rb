cb82c11a26701b5d928aaa952ce5c6e6df2829bf
Ben Treglown <treggy89@gmail.com>
2022-11-10 21:28:50 +1100

    completed app, WIP on front end

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,60 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  # Important info, You only need to make an instance variable if there
+  # is a 'view' associated with the method. Note there are no views for
+  # the !instance variables.
+
+  # -- Home --
+  def index
+    @tasks = Task.all
+  end
+
+  # -- Display one --
+  def show
+    set_task
+  end
+
+  # -- Create Task --
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.create(task_params)
+    task.save
+    # Redircet_to is a line of code that once the page loads it
+    # will redirect to another page straight away.
+    redirect_to task_path(tasks)
+  end
+
+  # -- Edit Task --
+  def edit
+    set_task
+  end
+
+  def update
+    set_task
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  # -- Delete --
+  def destroy
+    set_task
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  # We have a private function here that allows only the admin to change
+  # specific things in the code and not someone who can change it through
+  # the console
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,23 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # # -- Home --
+  # get 'tasks', to: 'tasks#index'
+  # # --
+  # # When creating a task, it must be above the 'show one' function
+  # # otherwise the order will break.
+  # # --
+  # # -- Creating a new task --
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # # -- Showing a singular task --
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # # -- Edit a task
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # -- Delete a task --
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
